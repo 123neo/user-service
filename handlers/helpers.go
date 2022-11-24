f6fb6855f21bce60a24d7110157825a48ef06e65
Prateek <prateek.agarwal@dltapps.co.uk>
2022-11-24 08:58:10 +0530

    NT: added Error JSON

--- handlers/helpers.go
+++ handlers/helpers.go
@@ -50,3 +50,17 @@ func encodeResponse(w http.ResponseWriter, status int, data interface{}, headers
 
 	return nil
 }
+
+func errorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return encodeResponse(w, statusCode, payload)
+}
